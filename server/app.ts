b0b0bcd2c76774ba46d4a93a5f9e619df58b9f36
Bohdan Tanchyk <tanchyk.bohdan@gmail.com>
2021-01-12 19:33:25 +0200

    TypeORM setup and starting the server

---
+++ server/app.ts
@@ -0,0 +1,38 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import express, {Errback, Request, Response, NextFunction, Application} from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from "helmet";
+
+const errorHandler = (err: Errback, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);
+    res.status(500).json({error: err});
+}
+
+createConnection({
+    type: 'postgres',
+    database: 'Notatboken',
+    username: 'postgres',
+    password: process.env.postgresPassword,
+    logging: true,
+    synchronize: true,
+    entities: []
+}).then(connection => {
+    const app: Application = express();
+
+    app.use(helmet());
+    app.use(cors());
+    app.use(express.urlencoded({extended: false}))
+    app.use(bodyParser.json());
+    app.use(errorHandler);
+
+    // app.use('/users', usersRouter);
+    // app.use('/posts', notesRouter);
+
+    const PORT = 5000;
+
+    app.listen(PORT, () => {
+        console.log(`PORT ${PORT}`)
+    });
+}).catch(error => console.log(error));
