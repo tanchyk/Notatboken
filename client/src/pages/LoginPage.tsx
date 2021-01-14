42207e9d5a988c5ff142305f77b8516eafbc9d4d
Bohdan Tanchyk <tanchyk.bohdan@gmail.com>
2021-01-14 16:35:08 +0200

    Login page design + routes for pages

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Box, Button, Input, InputGroup, InputRightElement, Stack} from "@chakra-ui/react";
+import {Box, Button, Heading, Image, Input, InputGroup, InputRightElement, Link, Stack} from "@chakra-ui/react";
+import {ExternalLinkIcon} from '@chakra-ui/icons'
 
 // interface InputData {
 //     username: string,
@@ -18,36 +19,66 @@ const LoginPage: React.FC<{}> = () => {
     const handleClick = () => setShow(!show)
 
     return (
-        <>
-            <Box maxW="lg" borderWidth="1px" borderRadius="lg" overflow="hidden" padding="20px" backgroundColor="#fff">
-            <Stack spacing={3}>
-                <Input variant="outline" placeholder="Email or Username" />
-                <InputGroup size="md">
-                    <Input
-                        pr="4.5rem"
-                        type={show ? "text" : "password"}
-                        placeholder="Enter password"
-                    />
-                    <InputRightElement width="4.5rem">
-                        <Button h="1.75rem" size="sm" onClick={handleClick}>
-                            {show ? "Hide" : "Show"}
+        <Stack
+            maxW={["100%", "90%", "70%", "50%"]}
+            borderWidth="1px"
+            borderRadius="lg"
+            overflow="hidden"
+            padding="20px"
+            backgroundColor="#fff"
+            margin="auto"
+            marginTop={["10px", "20px", "40px", "100px"]}
+        >
+            <Stack
+                direction="row"
+                wrap="wrap"
+                alignItems="center"
+                justifyContent="center"
+            >
+                <Stack spacing={5} textAlign="center">
+                    <Box boxSize={["sm", "sm", "sm", "md"]}>
+                        <Image
+                            src="https://image.freepik.com/vecteurs-libre/ordinateur-portable-smartphone-casque-cartoon-icon-illustration-concept-icone-technologie-entreprise-isole-style-bande-dessinee-plat_138676-2139.jpg"
+                        />
+                    </Box>
+                    <Link href="https://chakra-ui.com" isExternal>
+                        If you dont have account yet, register <ExternalLinkIcon mx="2px" />
+                    </Link>
+                </Stack>
+                <Stack spacing={5}>
+                    <Heading as="h1" size="xl">
+                        Login
+                    </Heading>
+
+                    <Input variant="outline" placeholder={"Email or Username"}/>
+                    <InputGroup size="md">
+                        <Input
+                            pr="4.5rem"
+                            type={show ? "text" : "password"}
+                            placeholder="Enter password"
+                        />
+                        <InputRightElement width="4.5rem">
+                            <Button h="1.75rem" size="sm" onClick={handleClick}>
+                                {show ? "Hide" : "Show"}
+                            </Button>
+                        </InputRightElement>
+                    </InputGroup>
+                    <Stack spacing={5} direction="row" alignItems="center">
+                        <Button
+                            width="120px"
+                            // isLoading={isSubmitting}
+                            type="submit"
+                            variantColor={'teal'}
+                        >
+                            Login
                         </Button>
-                    </InputRightElement>
-                </InputGroup>
+                        <Link color="blue.500" href="#">
+                            Home
+                        </Link>
+                    </Stack>
+                </Stack>
             </Stack>
-            <Box>
-                <Button
-                    mt={4}
-                    width="120px"
-                    // isLoading={isSubmitting}
-                    type="submit"
-                    variantColor={'teal'}
-                >
-                    Login
-                </Button>
-            </Box>
-            </Box>
-        </>
+        </Stack>
     );
 }
 
